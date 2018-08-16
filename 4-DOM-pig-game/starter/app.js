5c32a82a99bee86a76ad5a434560b49fa2b05d94
DUM00 <dumejo92@gmail.com>
2018-08-16 18:00:15 -0500

    final commit of the proyect

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,20 +9,34 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, gamePlaying;
+let scores, roundScore, activePlayer, gamePlaying, prevScore, maxScore;
 init();
 
+function generateDiceNumber() {
+    return Math.floor(Math.random()*6) + 1;
+}
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
     if (gamePlaying) {
         // Generate a random number
-        var dice = Math.floor(Math.random()*6) + 1;
+        var dice = generateDiceNumber();
+        var dice2 = generateDiceNumber();
         // Display the result
         let diceDOM = document.querySelector(".dice");
+        let diceDOM2 = document.querySelector(".dice2");
         diceDOM.style.display = "block";
+        diceDOM2.style.display = "block";
         diceDOM.src = "dice-" + dice + ".png";
+        diceDOM2.src = "dice-" + dice2 + ".png";
+        console.log(prevScore);
+        console.log(dice);
         // Update the round score IF the rolled number is different from 1
-        if (dice !== 1) {
+        if (dice === 6 && prevScore === 6){
+            scores[activePlayer] = 0;
+            document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+            nextPlayer();
+        }
+        else if (dice !== 1 && dice2 !== 1) {
             //Add score
             roundScore += dice;
             document.querySelector("#current-" + activePlayer).textContent = roundScore;
@@ -30,6 +44,7 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
             //Next Player turn
             nextPlayer();
         }
+        prevScore = dice;
     }
 })
 
@@ -49,9 +64,12 @@ document.querySelector(".btn-hold").addEventListener("click", function (){
 
         //Update the UI
         document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
-        
+        maxScore = document.getElementById("frm1").value;
+        if (!maxScore) {
+            maxScore = 100;
+        }
         //Check if the player won the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= maxScore) {
             document.querySelector("#name-" + activePlayer). textContent = "Winner!";
             document.querySelector(".dice").style.display = "none";
             document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
@@ -71,6 +89,7 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
     document.querySelector(".dice").style.display = "none";
+    document.querySelector(".dice2").style.display = "none";
     document.getElementById("score-0").textContent = "0";
     document.getElementById("score-1").textContent = "0";
     document.getElementById("current-0").textContent = "0";
